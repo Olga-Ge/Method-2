c1ac996fcc64710756e17eb5c9ea33604c37bc7a
user <olga.gezha@gmail.com>
2020-03-07 19:22:42 +0100

    1

--- HomeWork.py
+++ HomeWork.py
@@ -1,7 +1,14 @@
 fp= open("harry_potter.txt")
+fp2 = open ("harry_potter_2.txt", "w")
 list =[]
+fp.contents = fp.read()
 punct = "/!£$%&()=?^*+§°ç><,;:.-_{}"
+line = fp.contents.split()
 for line in fp:
     for p in punct:
         line =line.replace(p," ")
-        
+        fp.write(line)
+for x in line:
+    if x not in list:
+        list.append(x)
+        fp2.write(str(x)+"\n")
